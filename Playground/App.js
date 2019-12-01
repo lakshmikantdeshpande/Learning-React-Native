d37608374d244965e8a28a2caf926c48c84f71ff
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2019-12-01 23:12:32 +0530

    Created hello world

---
+++ Playground/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Bbye World!</Text>
+      </View>
+    );
+  }
+}
